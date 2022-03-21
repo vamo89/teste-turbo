62267b8646db968f4c1f1eadd567318f6e5e5be5
Victor Oliveira <vamo89@gmail.com>
2022-03-21 13:50:37 -0300

    Update to redirect work on dev

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,19 @@
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
 const withTM = require("next-transpile-modules")(["ui"]);
 
-module.exports = withTM({
-  reactStrictMode: true,
-});
+module.exports = (phase) =>
+  withTM({
+    reactStrictMode: true,
+    async rewrites() {
+      return [
+        {
+          source: "/docs/:match*",
+          destination:
+            phase === PHASE_DEVELOPMENT_SERVER
+              ? "http://localhost:3001/docs/:match*"
+              : "https://teste-turbo-docs.vercel.app/docs/:match*",
+        },
+      ];
+    },
+  });
